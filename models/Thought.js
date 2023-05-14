b07b37c86b3ce198f7c3fac3c8063fc7065409b2
AleksandrBausher <bausheraleksandr@gmail.com>
2023-05-14 16:52:24 -0400

    fixed thought model

--- models/Thought.js
+++ models/Thought.js
@@ -25,7 +25,50 @@ const thoughtSchema = new Schema(
     id: false,
   }
 );
+const { Schema, model } = require("mongoose");
 
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [//regex patters vaildaes email address
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);//virtual to get number of friends as this field is not in database
+userSchema.virtual("friendCount").get(function () {
+  return `${this.friends.length}`;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
 
   const Thought = model('thought', thoughtSchema);
 
